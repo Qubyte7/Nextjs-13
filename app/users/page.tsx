cf9acb9047bd3e67604c8b6d7736a62a44451712
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-09-30 10:00:55 +0200

    static and dynamic renderung

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const usersPage = () => {
+const usersPage = async() => {
+interface User{
+  id:number,
+  name:string
+
+}
+const res =await fetch('https://jsonplaceholder.typicode.com/users',{next:{revalidate:20}})
+const users:User[] = await res.json()
+
   return (
-    <div>usersPage</div>
+    <>
+    <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
+    <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
+    </>
   )
 }
 
