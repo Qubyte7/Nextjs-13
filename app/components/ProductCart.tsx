977c9e2224cc4fc1a9ea1157574cb36da5d37371
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-09-30 11:35:32 +0200

    using tailwind

--- app/components/ProductCart.tsx
+++ app/components/ProductCart.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart'
+import styles from './ProductCart.module.css'
 
 const ProductCart = () => {
   return (
-    <div>
+    <div className='p-5 my-5 bg-sky-300 hover:bg-sky-500 rounded-md text-xl text-white'>
       <AddToCart />
     </div>
   )
