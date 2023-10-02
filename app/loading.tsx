8004af02e4df6bbbad00a0b79fd489457821eefe
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-02 12:15:51 +0200

    loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div><span className="loading loading-spinner loading-lg"></span></div>
+  )
+}
+
+export default loading
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import Link from "next/link";
 
@@ -11,9 +11,10 @@ const usersPage = async ({searchParams:{sortOrder}}:Props) => {
   return (
     <>
       <h1>Users</h1>
-      <Link href="/users/new" className="btn">New User</Link>
-
-     <UserTable sortOrder={sortOrder}/>
+      <Link href="/users/new" className="btn">New User</Link> 
+    
+      <UserTable sortOrder={sortOrder}/>
+     
     </>
   );
 };
