f5a5175da3e791a2a29a7e9d6bc1637553ec5d25
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-03 16:03:23 +0200

    using zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,29 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
-export  function GET(request: NextRequest,{params}:{params : {id :number}}){
-    if (params.id > 10)
-       return NextResponse.json({error : 'User not found'},{status: 404})
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-    return NextResponse.json({id:1, name:"Shami"})
-}
-export async function PUT(request: NextRequest,{params}:{params : {id :number}}){
-    const body = await request.json();
-    if(!body.name)
-       return NextResponse.json({error:"Name is required"},{status:400})
-
-    if (params.id >10)
-        return NextResponse.json({error: 'User not found'},{status:404});
-    
-    return NextResponse.json({id:1,name: body.name})      
-
+  return NextResponse.json({ id: 1, name: "Shami" });
 }
 
-export async function DELETE(request: NextRequest,{params}:{params:{id:number}}){
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
 
-    if (params.id >10)
-        return NextResponse.json({error: 'User not found'},{status:404});
-  
-    return NextResponse.json({})
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
 
+  return NextResponse.json({ id: 1, name: body.name });
 }
- 
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
+}
