a2493098c293f8f3f728e71acd993e4d2df4d4c5
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-03 13:40:54 +0200

    deleting and updating in next js

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -6,3 +6,24 @@ export  function GET(request: NextRequest,{params}:{params : {id :number}}){
 
     return NextResponse.json({id:1, name:"Shami"})
 }
+export async function PUT(request: NextRequest,{params}:{params : {id :number}}){
+    const body = await request.json();
+    if(!body.name)
+       return NextResponse.json({error:"Name is required"},{status:400})
+
+    if (params.id >10)
+        return NextResponse.json({error: 'User not found'},{status:404});
+    
+    return NextResponse.json({id:1,name: body.name})      
+
+}
+
+export async function DELETE(request: NextRequest,{params}:{params:{id:number}}){
+
+    if (params.id >10)
+        return NextResponse.json({error: 'User not found'},{status:404});
+  
+    return NextResponse.json({})
+
+}
+ 
