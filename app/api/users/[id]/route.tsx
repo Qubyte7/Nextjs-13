f1cf6e8da0bb621ce9170aa2daff8106622b1793
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-04 19:33:55 +0200

    deleting and updating the user functionality

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -16,25 +16,43 @@ const user = await prisma.user.findUnique({
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
+const user = await prisma.user.findUnique({
+  where:{id:parseInt(params.id)}
+})
+    
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+const updatedUser = await  prisma.user.update({
+    where:{id: user.id},
+     data:{
+      name:body.name,
+      email:body.email
+     }
+  })
+  return NextResponse.json(updatedUser,{status:200});
 }
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({});
+  const user = await prisma.user.findUnique({
+    where:{id:parseInt(params.id)}
+  });
+  if (!user)
+    return NextResponse.json({ error: "User are trying to delete is not found" }, { status: 404 });
+    
+const deleteUser = await  prisma.user.delete({
+    where:{id:user.id}
+  })
+  return NextResponse.json({deleteUser});
 }
