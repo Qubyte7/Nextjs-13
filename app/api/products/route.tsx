039329b774813095b49387dca369be23dc6deba2
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-08 20:48:27 +0200

    Crud operation for users

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,18 +1,12 @@
 import { NextRequest,NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client1";
+
 export async function GET(req:NextRequest){
-    return NextResponse.json([
-        {
-            "id":1,
-            "name":"Milk",
-            "price":2.5
-        },
-        {
-            "id":2,
-            "name":"bread",
-            "price":3.5
-        }
-    ])
+const products = await prisma.product.findMany();
+return NextResponse.json(products);
+
+
 }
 
 export async function POST(req:NextRequest){
@@ -21,7 +15,14 @@ export async function POST(req:NextRequest){
     if(!validation.success){
         return NextResponse.json(validation.error.errors,{status:400})
     }
+ 
+ const createdProduct = await prisma.product.create({
+        data:{
+            name:body.name,
+            price:body.price,
+        }
+        })
 
-    return NextResponse.json({id:10, name: body.name, price: body.price},{status:201})
+    return NextResponse.json(createdProduct,{status:201})
 
-}
+    }
--- app/api/products/schema.ts
+++ app/api/products/schema.ts
@@ -2,7 +2,7 @@ import { z} from "zod"
 
 const schema  = z.object({
     name : z.string().min(3),
-    price: z.number().min(1).max(100)
+    price: z.number().min(1).max(1000)
 })
 
 
