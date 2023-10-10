f47d330fe880ef7621318f79453c95450adce032
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-10 15:34:15 +0200

    accessing the session on the client side

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+  //  console.log(session?.user);
+  
   return (
-    <div className='flex bg-slate-200 p-5 space-x-3'>
-<Link href="/" className='mr-5'>Next.js</Link>
-<Link href="/users">Users</Link>   
-<Link href="/api/auth/signin">Login</Link>   
- 
+    <div className="flex bg-slate-200 p-5 space-x-3">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users">Users</Link>
+      {status === 'loading' && <span className="loading loading-ring loading-xs"></span>}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
