b4fea2658493375defc8eabeff48caef474686c4
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-10 21:05:04 +0200

    accessing session on the server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import ProductCart from './components/ProductCart'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+const session = await getServerSession(authOptions)
+const name = session!.user!.name!.split(" ");
+const firstName = name[0];
   return (
     <main>
       
-      <h1>hello Shami</h1>
+      <h1>hello {session && <span>{firstName}</span>}</h1>
       {/* Link is a better way to specify links because it does not require the reload of all data on our website it simply change the UI to the link of the page linked to */}
       <Link href="/users">users</Link>
       <ProductCart />
