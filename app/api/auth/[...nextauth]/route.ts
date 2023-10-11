c12a40bf760206a7b418c3c4a3a20fc43320197b
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-11 21:38:51 +0200

    signining using github

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,56 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import  prisma  from "../../../../prisma/client1";
-import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import prisma from "../../../../prisma/client1";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+
 
 export const authOptions: NextAuthOptions = {
-    adapter:PrismaAdapter(prisma),
-    providers:[
-        CredentialsProvider({
-         name: 'Credentials',
-         credentials:{
-            email:{label: 'Email' , type: 'email', placeholder:"Email..."},
-            password:{label: 'Password' , type: 'password', placeholder:"Password..."}
-         },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email..." },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password...",
+        },
+      },
 
-        async authorize(credentials,req){
-            if(!credentials?.email || !credentials?.password) return null;
-            const user = await prisma.user.findUnique({where: {email:credentials.email}})
-        
-            if(!user) return null;
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
 
-           const passwordsMatch = await bcrypt.compare(credentials.password,user.hashedPassword!);
-           return passwordsMatch ? user : null;
-        
-        }
-        }),
-        GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID!,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
-        })
-    ],
-    session:{
-        strategy:'jwt'
-    }
-}
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+        return passwordsMatch ? user : null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking:true
+    }),
+    GithubProvider({
+        clientId:process.env.GITHUB_CLIENT_ID!,
+        clientSecret:process.env.GITHUB_CLIENT_SECRET!,
+        allowDangerousEmailAccountLinking:true
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
 const handler = NextAuth(authOptions);
-export {handler as GET ,handler as POST}
+export { handler as GET, handler as POST };
--- prisma/migrations/20231011171849_first_migration/migration.sql
+++ prisma/migrations/20231011171849_first_migration/migration.sql
@@ -33,6 +33,7 @@ CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NULL,
+    `hashedPassword` VARCHAR(191) NULL,
     `emailVerified` DATETIME(3) NULL,
     `image` VARCHAR(191) NULL,
 
