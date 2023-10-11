e7ad114a7ed85748f3d578cb3414c306649e02c2
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-10-11 06:45:05 +0200

    using Prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import  prisma  from "../../../../prisma/client1";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+    adapter:PrismaAdapter(prisma),
     providers:[
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID!,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    session:{
+        strategy:'jwt'
+    }
 }
 const handler = NextAuth(authOptions);
 export {handler as GET ,handler as POST}
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
 export { default} from 'next-auth/middleware'//this synta is for  exporting the middleware form the path 'next-auth/middleware' 
 
 //specifying endpoints where our middleware should be applied
